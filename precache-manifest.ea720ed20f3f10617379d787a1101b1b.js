4fa966cf92f9a721a210ff94bf0d3673a7a94b34
Ankit Jilka <ankitjilka10@gmail.com>
2022-08-02 15:27:29 +0530

    Updates

---
+++ precache-manifest.ea720ed20f3f10617379d787a1101b1b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "96778b37c17c379549b2",
+    "url": "/Watch-Mart/static/css/main.be7922c3.chunk.css"
+  },
+  {
+    "revision": "96778b37c17c379549b2",
+    "url": "/Watch-Mart/static/js/main.96778b37.chunk.js"
+  },
+  {
+    "revision": "4e9aafb99959cf9fe670",
+    "url": "/Watch-Mart/static/js/1.4e9aafb9.chunk.js"
+  },
+  {
+    "revision": "e0c3249284c32c7077b2",
+    "url": "/Watch-Mart/static/css/2.6a82314e.chunk.css"
+  },
+  {
+    "revision": "e0c3249284c32c7077b2",
+    "url": "/Watch-Mart/static/js/2.e0c32492.chunk.js"
+  },
+  {
+    "revision": "2d8aaa48dd3cd94428e5",
+    "url": "/Watch-Mart/static/js/runtime~main.2d8aaa48.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Watch-Mart/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Watch-Mart/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Watch-Mart/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Watch-Mart/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Watch-Mart/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "2e59600f93246d88554f41fb11350619",
+    "url": "/Watch-Mart/index.html"
+  }
+];
